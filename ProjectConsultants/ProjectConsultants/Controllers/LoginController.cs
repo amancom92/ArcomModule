9ba3248e523085623d2978ad1b46d4dce8760523
Aman Mehta <13087@IIPL.COM>
2016-05-19 17:53:06 +0530

    Login Functionality Completed

---
+++ ProjectConsultants/ProjectConsultants/Controllers/LoginController.cs
@@ -0,0 +1,45 @@
+﻿using ProjectConsultants.UI.ViewModel;
+using System;
+using System.Net.Http;
+using System.Web.Mvc;
+
+namespace ProjectConsultants.Controllers
+{
+    public class LoginController : BaseController
+    {
+        /// <summary>
+        /// Logins this instance.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Logins the specified login view model.
+        /// </summary>
+        /// <param name="loginViewModel">The login view model.</param>
+        /// <returns></returns>
+        //[HttpPost]
+        public ActionResult Login(LoginViewModel loginViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri("http://localhost:63465/");
+
+                HttpResponseMessage response = client.GetAsync("api/Login/AuthenticateLogin?UserName=" + loginViewModel.UserName + "&Password=" + loginViewModel.Password).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToActionPermanent("Index", "Project");
+                }
+            }
+            return View(loginViewModel);
+        }
+
+    }
+}
--- ProjectConsultants/ProjectConsultants/Controllers/ProjectController.cs
+++ ProjectConsultants/ProjectConsultants/Controllers/ProjectController.cs
@@ -1,5 +1,4 @@
-﻿using ProjectConsultants.Entity;
-using ProjectConsultants.UI.ViewModel;
+﻿using ProjectConsultants.UI.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -41,7 +40,7 @@ namespace ProjectConsultants.Controllers
         {
 
             HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("http://localhost:63465/");
+            client.BaseAddress = new Uri("http://localhost:52338/");
 
             HttpResponseMessage response = client.GetAsync("api/Common/GetCountryList").Result;
             if (response.IsSuccessStatusCode)
@@ -171,7 +170,7 @@ namespace ProjectConsultants.Controllers
             List<SelectListItem> states = new List<SelectListItem>();
 
             HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("http://localhost:63465/");
+            client.BaseAddress = new Uri("http://localhost:52338/");
             //set the Content-Type to application/json
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
            
@@ -195,7 +194,7 @@ namespace ProjectConsultants.Controllers
             List<SelectListItem> ownerStates = new List<SelectListItem>();
 
             HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("http://localhost:63465/");
+            client.BaseAddress = new Uri("http://localhost:52338/");
             //set the Content-Type to application/json
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
