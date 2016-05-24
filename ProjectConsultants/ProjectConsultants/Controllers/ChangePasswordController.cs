41898fd561ae234d5df9e99c8069bc86d81027d3
Prateek Prakash <13090@IIPL.COM>
2016-05-24 12:45:14 +0530

    Files created after merging.

--- ProjectConsultants/ProjectConsultants/Controllers/ChangePasswordController.cs
+++ ProjectConsultants/ProjectConsultants/Controllers/ChangePasswordController.cs
@@ -1,5 +1,4 @@
-﻿using ProjectConsultants.Action;
-using ProjectConsultants.UI.ViewModel;
+﻿using ProjectConsultants.UI.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
